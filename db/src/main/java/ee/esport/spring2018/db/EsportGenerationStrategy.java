305e38ae1057b523a3c6ed29e5e723702b391d09
Jaagup Kümmel <jaagup.kymmel@nortal.com>
2017-12-26 18:34:57 +0200

    Renamed DB tables to snake_case, because Windows is terrible

--- db/src/main/java/ee/esport/spring2018/db/EsportGenerationStrategy.java
+++ db/src/main/java/ee/esport/spring2018/db/EsportGenerationStrategy.java
@@ -1,14 +1,10 @@
 package ee.esport.spring2018.db;
 
+import org.jooq.util.ColumnDefinition;
 import org.jooq.util.DefaultGeneratorStrategy;
 import org.jooq.util.Definition;
 
 public class EsportGenerationStrategy extends DefaultGeneratorStrategy {
-
-    @Override
-    public String getJavaClassName(Definition definition, Mode mode) {
-        return capitilize(definition.getOutputName() + "Record");
-    }
 
     private String capitilize(String string) {
         return Character.toUpperCase(string.charAt(0)) + string.substring(1);
@@ -16,7 +12,19 @@ public class EsportGenerationStrategy extends DefaultGeneratorStrategy {
 
     @Override
     public String getJavaIdentifier(Definition definition) {
-        return definition.getOutputName().replaceAll("(.+)(\\p{javaUpperCase})", "$1_$2").toUpperCase();
+        if(definition instanceof ColumnDefinition) {
+            return definition.getOutputName().replaceAll("(.+)(\\p{javaUpperCase})", "$1_$2").toUpperCase();
+        }
+        return super.getJavaIdentifier(definition);
     }
 
+    @Override
+    public String getJavaSetterName(Definition definition, Mode mode) {
+        return "set" + capitilize(definition.getOutputName());
+    }
+
+    @Override
+    public String getJavaGetterName(Definition definition, Mode mode) {
+        return "get" + capitilize(definition.getOutputName());
+    }
 }
---
+++ db/src/main/resources/db/migration/V0.1514303778__rename_tables_to_snakecase.sql
@@ -0,0 +1,3 @@
+-- Windows sucks
+RENAME TABLE Tickets to TICKETS;
+RENAME TABLE TicketTypes to TICKET_TYPES;
