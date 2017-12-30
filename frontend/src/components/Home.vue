d2b6b1d89eff09feb5e11268bad9835719234986
Johannes Kümmel <jkymmel@gmail.com>
2017-12-30 15:02:51 +0200

    more sponsors

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -22,10 +22,23 @@
     .container.content-block
       h1.text-center Sponsorid
       .row.is-flex.sponsors
-        .col-lg-6.col-sm-12: img.img-responsive(src="../assets/sponsors/arvutitark_logo.svg")
-        .col-lg-6.col-sm-12: img.img-responsive(src="../assets/sponsors/msi-gaming.png")
-        .col-lg-6.col-sm-12: img.img-responsive(src="../assets/sponsors/itt.png")
-        .col-lg-6.col-sm-12: img.img-responsive(src="../assets/sponsors/ituk.svg")
+        .col-xs-6: img.img-responsive(src="../assets/sponsors/arvutitark_logo.svg")
+        .col-xs-6: img.img-responsive(src="../assets/sponsors/msi-gaming.png")
+        .col-xs-6: img.img-responsive(src="../assets/sponsors/itt.png")
+        .col-xs-6: img.img-responsive(src="../assets/sponsors/ituk.svg")
+      .row.is-flex.sponsors-sm
+        .col-xs-3: img(src="../assets/sponsors/noctua.jpg")
+        .col-xs-3: img(src="../assets/sponsors/roccat.jpg")
+        .col-xs-3: img(src="../assets/sponsors/balsnack.svg")
+        .col-xs-3: img(src="../assets/sponsors/wolt.png")
+        .col-xs-3: img(src="../assets/sponsors/boommedia.png")
+        .col-xs-3: img(src="../assets/sponsors/filmiklubi-03.svg")
+        .col-xs-3: img(src="../assets/sponsors/monster-02.svg")
+        .col-xs-3: img(src="../assets/sponsors/network_tomorrow.png")
+        .col-xs-3: img(src="../assets/sponsors/academic_hostel.svg")
+        .col-xs-3: img(src="../assets/sponsors/bytelife.png")
+        .col-xs-3: img(src="../assets/sponsors/exit_room.jpg")
+        .col-xs-3: img(src="../assets/sponsors/iveco.jpg")
 </template>
 
 <script>
