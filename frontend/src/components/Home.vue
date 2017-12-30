abd863ab47fbfec5782ca1abfcfe16d4170a0fed
Johannes Kümmel <jkymmel@gmail.com>
2017-12-30 14:11:02 +0200

    added main sponsors and a ticket example

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -7,8 +7,25 @@
             span
               h1 9.-11. veebruar
               h1 CSGO
-    .container
-      .h1 Hello!
+    .container.content-block
+      h1.text-center Piletid
+      .row
+        .col-lg-4.col-md-6.col-xs-12
+          .ticket
+            .ticket-content
+              h2.ticket-name Early Bird CSGO
+              h3.ticket-price 15€
+              p.ticket-remaining 15tk alles
+              p.ticket-description Saadaval kuni 05. jaanuar
+            //router-link.buy-btn(to="/") Osta
+            span.buy-btn.disabled Osta
+    .container.content-block
+      h1.text-center Sponsorid
+      .row.is-flex.sponsors
+        .col-lg-6.col-sm-12: img.img-responsive(src="../assets/sponsors/arvutitark_logo.svg")
+        .col-lg-6.col-sm-12: img.img-responsive(src="../assets/sponsors/msi-gaming.png")
+        .col-lg-6.col-sm-12: img.img-responsive(src="../assets/sponsors/itt.png")
+        .col-lg-6.col-sm-12: img.img-responsive(src="../assets/sponsors/ituk.svg")
 </template>
 
 <script>
