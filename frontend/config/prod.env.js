0ead29ca4600c07a00985714160c4cadaa19ec66
Jaagup Kümmel <jaagup.kymmel@nortal.com>
2018-01-02 19:30:21 +0200

    fIXED PROD CONFIG

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = {
   NODE_ENV: '"production"',
-  apiBase: 'https://e-sport.ee',
-  steamLoginReturnTo: 'https://e-sport.ee/static/verifySteamLogin.html'
+  apiBase: '"https://e-sport.ee"',
+  steamLoginReturnTo: '"https://e-sport.ee/static/verifySteamLogin.html"'
 };
