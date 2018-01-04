05a0c8623f0cf8ce5e05e50811e4f56d1e6273ec
Johannes Kümmel <jkymmel@gmail.com>
2018-01-04 20:36:58 +0200

    translations

--- frontend/src/components/public/Faq.vue
+++ frontend/src/components/public/Faq.vue
@@ -5,6 +5,11 @@
       .faq(v-for="i in 7")
         p: strong(v-t="'faq.questions.' + i + '.q'")
         p(v-t="'faq.questions.' + i + '.a'")
+      .faq
+        p: strong(v-t="'faq.questions.8.q'")
+        p {{ $t('faq.questions.8.a') }}&nbsp
+          a(:href="'/static/' + $t('faq.questions.8.file')") {{ $t('faq.questions.8.parentalPermit') }}
+          | .
 </template>
 
 <script>
