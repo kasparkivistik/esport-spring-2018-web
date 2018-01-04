93c74f39ccb093b20e5c4b8a9947c08ae00ba92e
Johannes Kümmel <jkymmel@gmail.com>
2018-01-04 19:41:01 +0200

    improved mytickets

--- frontend/src/components/public/MyTickets.vue
+++ frontend/src/components/public/MyTickets.vue
@@ -1,23 +1,19 @@
 <template lang="pug">
   .container
     h1.text-center(v-t="'tickets.tickets'")
-    table.table.table-bordered.table-striped
-      thead
-        tr
-          th(v-t="'tickets.id'")
-          th(v-t="'tickets.name'")
-          th(v-t="'tickets.type'")
-          th(v-t="'tickets.status'")
-          th(v-t="'tickets.boughtOnDate'")
-          th(v-t="'tickets.price'")
-      tbody
-        tr(v-for="ticket in tickets")
-          td {{ticket.id}}
-          td {{ticket.name}}
-          td {{ $t('tickets.names["' + ticket.type.name + '"]') }}
-          td {{ $t('tickets.statuses["' + ticket.status + '"]') }}
-          td {{ticket.dateCreated | moment("Do MMMM") }}
-          td {{ticket.type.cost}} €
+    blockquote.my-ticket(v-for="ticket in tickets")
+      p.lead {{ $t('buy.name') }}:
+        span.text-primary  {{ticket.name}}
+      p: small.text-default {{ $t('buy.invoiceNumber') }}:
+        span.text-primary  {{getInvoiceNumber(ticket.id)}}
+      p: small.text-default {{ $t('buy.ticket') }}:
+        span.text-primary  {{ $t('tickets.names["' + ticket.type.name + '"]') }}
+      p: small.text-default {{ $t('tickets.status') }}:&nbsp
+        span.label(:class="getStatusClass(ticket.status)")  {{ $t('tickets.statuses["' + ticket.status + '"]') }}
+      p: small.text-default {{ $t('tickets.boughtOn') }}:
+        span.text-primary  {{ticket.dateCreated | moment("Do MMMM HH:mm") }}
+      p {{ $t('buy.total') }}:
+        span.text-primary  {{ticket.type.cost}} €
 
 </template>
 
@@ -28,6 +24,25 @@
       return {
         tickets: null
       };
+    },
+    methods: {
+      getInvoiceNumber: function (id) {
+        return '2018-359027-' + '000'.substring(0, 3 - id.toString().length) + id.toString();
+      },
+      getStatusClass: function (status) {
+        switch (status) {
+          case 'IN_WAITING_LIST':
+            return 'label-warning';
+          case 'AWAITING_PAYMENT':
+            return 'label-info';
+          case 'PAID':
+            return 'label-success';
+          case 'CANCELED':
+            return 'label-danger';
+          default:
+            return 'label-primary';
+        }
+      }
     },
     mounted: function () {
       const self = this;
